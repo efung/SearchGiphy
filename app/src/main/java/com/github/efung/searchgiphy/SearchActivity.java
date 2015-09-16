aa6c97023b709c2042d69e157ca38b5f6efbb810
Eric Fung <eric.fung@shopify.com>
2015-09-16 07:07:32 -0400

    Initial project and main UI

---
+++ app/src/main/java/com/github/efung/searchgiphy/SearchActivity.java
@@ -0,0 +1,26 @@
+package com.github.efung.searchgiphy;
+
+import android.support.v4.view.MenuItemCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.SearchView;
+
+public class SearchActivity extends AppCompatActivity {
+    private SearchView searchView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        searchView = (SearchView) toolbar.findViewById(R.id.search);
+        searchView.setSubmitButtonEnabled(true);
+    }
+
+}
